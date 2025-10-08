d8b85ec61367372c9b1dbe4d283abe118039ca10
Sasha Hurbanava <sasha.hurbnva@gmail.com>
2025-10-08 15:45:32 +0300

    fix: delete wrong import

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import * as ReactDom from 'react-dom/client';
 
 import App from './App';
 
-import './styles/style.scss';
-
 const container = document.getElementById('root');
 
 if (!container) throw new Error('Root container not found');
