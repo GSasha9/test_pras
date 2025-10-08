b89d66b65631c65390acfeef8922c0e8aaceead0
Sasha <sasha.hurbnva@gmail.com>
2025-10-08 20:47:00 +0300

    feat: complete Banner component along with its styles

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -10,10 +10,23 @@ const Banner = ({ primaryText, secondaryText, imgLink }: BannerProps) => {
   return (
     <div
       className={styles.banner__wrapper}
-      style={{ backgroundImage: `url(${imgLink})` }}
+      style={{
+        backgroundImage: `
+          linear-gradient(
+            to bottom,
+            rgba(0, 0, 0, 0.6) 0%,
+            rgba(0, 0, 0, 0.3) 40%,
+            rgba(0, 0, 0, 0.3) 60%,
+            rgba(0, 0, 0, 0.6) 100%
+          ),
+          url(${imgLink})
+        `,
+      }}
     >
-      <p>#{secondaryText}</p>
-      <h2>{primaryText}</h2>
+      <div className={styles.banner__text}>
+        <p className={styles.banner__text_secondary}>#{secondaryText}</p>
+        <h2 className={styles.banner__text_primary}>{primaryText}</h2>
+      </div>
     </div>
   );
 };
